bd70b3f2cab8724796eca57dcdae5025a89f7355
Lwrnc U.B <murshyne@gmail.com>
2024-10-02 03:04:59 -0400

    Backup at:

---
+++ index.js
@@ -0,0 +1,36 @@
+let log = console.log.bind(console);
+
+// log("Hello");
+// log("I like Pizza");
+// alert("Hey");
+// alert("This is an Alert!");
+
+document.getElementById("myH1").textContent = 'Hello';
+document.getElementById("myP").textContent = 'I like Pizza';
+
+
+// /**
+//  * @param {string} s
+//  * @return {number}
+//  */
+let age = 25;
+let y = 856;
+let gas = 3.50  
+log(`you are ${gas} old`)
+
+console.log();
+
+function functName() {
+    console.log();
+
+}
+
+const greet = function(name) {
+    console.log("");
+
+}
+
+let i = 5;
+for (let i = 0; i < 10; i++) {
+  // some statements
+  };
